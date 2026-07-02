a9e69c3a80854184f39f85b5ef6026a890ee7eff
pgnox1st <s78399616@gmail.com>
2026-07-02 19:13:40 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,1 +1,16 @@
-// vite config
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true
+  }
+})
+    
