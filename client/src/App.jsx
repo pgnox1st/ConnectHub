8d9311b8447d4083261d906f732763f2c7e2e27f
pgnox1st <s78399616@gmail.com>
2026-07-02 10:34:55 +0530

    Add welcome message in Hindi to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,2 +1,14 @@
-// App Component Code
+import React from 'react'
 
+function App() {
+  return (
+    <div className="app-container">
+      <div style={{ padding: '20px', textAlign: 'center', width: '100%' }}>
+        <h1>ConnectHub में आपका स्वागत है!</h1>
+        <p>आपकी सोशल मीडिया वेबसाइट का बेस सफलतापूर्वक तैयार हो गया है।</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
