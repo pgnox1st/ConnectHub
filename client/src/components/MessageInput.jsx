8a01d09edda8089120745d33246118a8de32cab7
pgnox1st <s78399616@gmail.com>
2026-07-03 20:54:06 +0530

    Update MessageInput.jsx

--- client/src/components/MessageInput.jsx
+++ client/src/components/MessageInput.jsx
@@ -1,1 +1,28 @@
+import React from "react";
 
+function MessageInput() {
+  return (
+    <footer className="inputBox">
+
+      <button title="Attach File">
+        📎
+      </button>
+
+      <input
+        type="text"
+        placeholder="Message pgnox1st AI..."
+      />
+
+      <button title="Voice">
+        🎤
+      </button>
+
+      <button title="Send">
+        ➤
+      </button>
+
+    </footer>
+  );
+}
+
+export default MessageInput;
