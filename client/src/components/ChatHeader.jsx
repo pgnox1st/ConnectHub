ac1c18650d8cf34d7d054cad6c4821d3771867cd
pgnox1st <s78399616@gmail.com>
2026-07-03 21:24:41 +0530

    Update ChatHeader.jsx

--- client/src/components/ChatHeader.jsx
+++ client/src/components/ChatHeader.jsx
@@ -1,1 +1,20 @@
+import React from "react";
 
+function ChatHeader() {
+  return (
+    <header className="chatHeader">
+
+      <h2>ConnectHub</h2>
+
+      <div className="headerActions">
+        <button>🔍</button>
+        <button>📞</button>
+        <button>🎥</button>
+        <button>⋮</button>
+      </div>
+
+    </header>
+  );
+}
+
+export default ChatHeader;
