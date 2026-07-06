546e9bff28f569307924f07965fb41576016527b
pgnox1st <s78399616@gmail.com>
2026-07-06 15:57:38 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,39 +3,31 @@ import "./App.css";
 
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
-import FeatureCards from "./components/FeatureCards/FeatureCards";
+import FeatureCards from "./components/FeatureCards.jsx";
 import ChatWindow from "./components/ChatWindow/ChatWindow";
 
 function App() {
   return (
     <div className="app">
-
       <Sidebar />
 
       <div className="main">
-
         <Header />
 
         <div className="home">
-
           <div className="welcome">
-
             <h1>
               Hello, <span>User 👋</span>
             </h1>
 
             <p>How can I help you today?</p>
-
           </div>
 
           <FeatureCards />
 
           <ChatWindow />
-
         </div>
-
       </div>
-
     </div>
   );
 }
