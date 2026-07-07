3960783b4b7b7703d97156ac4a2315dbc407d069
pgnox1st <s78399616@gmail.com>
2026-07-07 09:50:34 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,1 +1,38 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      reply: "Method Not Allowed",
+    });
+  }
+
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(400).json({
+        reply: "Message is required",
+      });
+    }
+
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+    const model = genAI.getGenerativeModel({
+      model: "gemini-2.5-flash",
+    });
+
+    const result = await model.generateContent(message);
+
+    return res.status(200).json({
+      reply: result.response.text(),
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      reply: "AI is currently unavailable.",
+    });
+  }
+      }
